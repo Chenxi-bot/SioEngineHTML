e9754c0d9aae0ab12010d2f5d53392099905d050
Chenxi <vinsarup@gmail.com>
2022-05-12 18:09:40 +0800

    1.0.0

---
+++ js/controller.js
@@ -0,0 +1,26 @@
+// 当引擎初始化时，会创建一个controller对象，该对象将管理游戏大部分内容
+
+function readFile(path) {
+    const fs = require('fs');
+    var data = fs.readFileSync(path);
+    return data.toString();
+}
+
+var controller = {};    // 引擎全局控制，游戏流程由这一个管理
+
+controller.init = function () {
+
+    // 版本类，不影响流程进行，主要是作者进行版本控制与签名
+    this.name = "UnLoad"; // 游戏名称
+    this.version = "1.0.0";   // 游戏版本
+    this.author = "Anonymous";    // 游戏作者
+
+    // 流程类，主要是存档相关，和游戏进程有关，以及一些奇奇怪怪的东西
+    this.firstRead = false;   // 是否首次游玩，可以用于演出
+
+}
+
+controller.init();
+console.log(controller);
+
+// 吃饭去了，回来继续

9fb0786d697f8203ca044856dfd59c92d3c0d3af
Chenxi <vinsarup@gmail.com>
2022-05-25 22:00:31 +0800

    睡前改了一点，不过内容不多就不新启一个版本了

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,7 @@
 // 当引擎初始化时，会创建一个controller对象，该对象将管理游戏大部分内容
 
 class Controller {
+
     constructor() {
 
         // 版本类，不影响流程进行，主要是作者进行版本控制与签名
@@ -11,15 +12,22 @@ class Controller {
 
         // 流程类，主要是存档相关，和游戏进程有关，以及一些奇奇怪怪的东西
         this.firstRead = false; // 是否首次游玩，可以用于演出
-        this.charpeter = 0;
-        this.line = 0;
-        this.clickId = undefined;
+        this.charpeter = 0; // 当前阅读的章数
+        this.line = 0;  // 当前阅读的行数
+        this.save
 
-        // 内容类，主要是游戏内容
-        this.scenario;
+
+        // 内容类，主要是游戏内容相关
+
+
+        // 功能类，
+        this.scenario;  // 剧本，重中之重
+        this.clickId = undefined;   // 记录播放的循环id，控制左键点击事件
+        this.decoration = false;    // 文字装饰器，算是保留功能，是delay()里面的decoration()函数
 
         return this;
     }
+
 
     // 工具函数类
     loadScenario(path) {    // 加载剧本
@@ -28,12 +36,11 @@ class Controller {
             controller.scenario = JSON.parse(xhr.responseText);
 
             function load() {
-                console.log("load");
                 controller.name = controller.scenario.name;
                 document.title = controller.name;
-                console.log("rename");
             };
             load();
+            delay();
         };
         try {
             xhr.open("get", path);
@@ -46,22 +53,71 @@ class Controller {
 }
 
 // 人物图层，作为子对象方便管理
+// 结果用处并不大，不如不用
 class imageLayer {
-    constructor(src) {
-        this.ele = document.createElement("img");
+
+    constructor(src, name) {
+        this.ele = document.createElement("div");
+        this.img = document.createElement("img");
+        this.ele.appendChild(this.img);
         this.index = 0;
-        this.ele.src = src;
+        if (src != undefined)
+            this.img.src = src;
+        if (name != undefined) {
+            this.ele.id = name;
+        }
     }
 }
 
 // 图层管理器，功能正在完成中
 class imageController {
+
     constructor() {
-        this.layers = [];
         this.fgimage = document.getElementById("fgiamge");
     }
-    addImage(src, posx, posy) {
 
+    // addImage()和deleteImage()可以使用css动画来实现
+    // 不过感觉用js写应该比css简单
+    addImage(src, posx, posy, name) {
+        let img = new imageLayer(src, name);
+        let fgimage = document.getElementById("fgimage");
+        fgimage.appendChild(img.ele);
+        img.img.style.position = "absolute";
+        img.img.style.marginLeft = String(posx) + "px";
+        img.img.style.marginTop = String(posy) + "px";
+        return ("图层 " + name + " 已创建，" + "路径：" + src);
+    }
+
+
+    deleteImage(name) {
+        let ele = document.getElementById(name);
+        ele.remove();
+        return ("图层 " + name + " 已删除");
+    }
+
+    
+    // replaceImage()为最终接口，详细见注释
+    // 替换图片，按照名字替换，位置参数可选，相关动画以后再做
+    replaceImage(name, src, posx, posy) {
+        let ele = document.getElementById(name);
+
+        // 如果当前不存在id为name的图层，调用addImage()函数
+        if (ele == null)
+            return this.addImage(src, posx, posy, name);
+        
+        // 如果并没有传入src参数，调用deleteImage()函数
+        if (src == undefined)
+            return this.deleteImage(name);
+        
+        ele.getElementsByTagName("img")[0].src = src;
+
+        // 检查是否有pos参数传入，没有就不变
+        if (posx != undefined)
+            ele.getElementsByTagName("img")[0].style.marginLeft = String(posx) + "px";
+        if (posy != undefined)
+            ele.getElementsByTagName("img")[0].marginTop = String(posy) + "px"
+        
+        return ("图层 " + name + " 已替换，" + "路径：" + src);
     }
 }
 
@@ -69,9 +125,11 @@ class imageController {
 var controller = new Controller();
 controller.loadScenario("https://sioengine.oss-cn-beijing.aliyuncs.com/scenario/test.json");
 
+// 实例化图层管理器
+var iCtrl = new imageController();
 
-// 动画类，需要等整个网页加载完成以后使用
-window.onload = function Anime() {
+// 延迟类，需要等整个网页加载完成以后使用
+function delay() {
 
     function click_anime() {    // 播放动画，每个字逐个显示出来
         let info = "";
@@ -83,15 +141,22 @@ window.onload = function Anime() {
         controller.clickId = setInterval(function () {
             document.getElementById("text").innerHTML = sub;
             sub = info.substring(0, now);
-            if (now++ > length)
-            {
+            if (now++ > length) {
                 clearInterval(controller.clickId);
                 controller.clickId = undefined;
             }
         }, 50);
     }
 
-    document.body.addEventListener("click", function () {   // 监听左键动作，进行动画播放
+    function decoration() { // 装饰函数，用于特殊的装饰，比如人名/语句的引号什么的
+        if (controller.decoration && (arguments[0].name != "")) {
+            arguments[0].name = "【" + arguments[0].name + "】";
+            arguments[0].text = "『" + arguments[0].text + "』";
+        }
+        else return arguments[0];
+    }
+
+    function left_click() {   // 动画播放
 
         // 如果当前有正在播放的动画，立即结束动画并直接显示全部内容
         // 等效于左键双击
@@ -108,14 +173,34 @@ window.onload = function Anime() {
         if (controller.line++ < controller.scenario.length) {
             document.getElementById("text").innerHTML = "";
             // console.log("nowLine: ", controller.line);
+            decoration(controller.scenario.content[controller.line]);   // 装饰器（迫真）预处理
             click_anime(controller.scenario.content[controller.line].text);
             document.getElementById("name").innerHTML = controller.scenario.content[controller.line].name;
+
+            // 关于图片切换
+            // 要做的东西有点多
+            if (controller.scenario.content[controller.line].anime.picfp != "") {
+                let info = 
+                iCtrl.replaceImage(
+                    controller.scenario.content[controller.line].anime.name,
+                    controller.scenario.content[controller.line].anime.picfp,
+                    controller.scenario.content[controller.line].anime.posx,
+                    controller.scenario.content[controller.line].anime.posy);
+                console.log(info);
+            }
         }
 
-    });
+        // 如果ui隐藏，显示ui
+        let uiLayer = document.getElementById("ui");
+        if (uiLayer.style.visibility == "hidden")
+            uiLayer.style.visibility = "visible";
 
-    document.oncontextmenu = function () {  // 标准右键动作，隐藏ui和文字层
-        // console.log("right click");
+
+    }
+
+    document.body.addEventListener("click", left_click);    // 监听左键动作
+
+    document.oncontextmenu = function () {  // 标准右键动作，隐藏ui和文字层，同时取消默认菜单
         let uiLayer = document.getElementById("ui");
         if (uiLayer.style.visibility == "visible")
             uiLayer.style.visibility = "hidden";
@@ -124,5 +209,8 @@ window.onload = function Anime() {
         return false;
     };
 
-
+    document.addEventListener("wheel", function (event) {
+        if (event.deltaY > 0)
+            left_click();
+    });
 }
